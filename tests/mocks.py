d46be4210eb5fafdb05993e1e887553cff128431
Patrick Menschel <menschel.p@posteo.de>
2021-02-16 21:54:44 +0100

    Refactor mocks to reduce complexity

--- tests/mocks.py
+++ tests/mocks.py
@@ -6,7 +6,8 @@
 
 from serial import Serial
 
-from hcscom.hcscom import split_data_to_values, OutputStatus, FORMAT_THREE_DIGITS, format_to_width_and_decimals, format_val
+from hcscom.hcscom import split_data_to_values, OutputStatus, FORMAT_THREE_DIGITS, format_to_width_and_decimals, \
+    format_val
 
 
 class HcsMock(Serial):
@@ -27,67 +28,78 @@ class HcsMock(Serial):
         self.width, self.decimals = format_to_width_and_decimals(self.value_format)
         self.max_voltage = 32.2
         self.max_current = 20.2
+        self.get_commands = {"GMAX": None,
+                             "GETS": None,
+                             "GETD": None,
+                             "GETM": None,
+                             "GOVP": None,
+                             "GOCP": None,
+                             }
+        self.set_commands = {"SOUT": None,
+                             "VOLT": None,
+                             "CURR": None,
+                             "RUNM": None,
+                             "SOVP": None,
+                             "SOCP": None,
+                             }
+        super().__init__()
 
-        super().__init__()  # do we need this for a mock ?
-        #self.is_open = True
+    def handle_sets(self, command, value_data):
+        if command == "SOUT":
+            value = int(value_data[0])
+            assert value in [OutputStatus.off, OutputStatus.on]
+            self.output_status = value
+        elif command == "VOLT":
+            values = split_data_to_values(data=value_data, width=self.width, decimals=self.decimals)
+            assert len(values) == 1
+            self.active_preset[0] = values[0]
+        elif command == "CURR":
+            values = split_data_to_values(data=value_data, width=self.width, decimals=self.decimals)
+            assert len(values) == 1
+            self.active_preset[1] = values[0]
+        elif command == "RUNM":
+            value = int(value_data[0])
+            self.active_preset = self.presets[value]
+            self.display_values = self.presets[value]
+        elif command == "SOVP":
+            values = split_data_to_values(data=value_data, width=self.width, decimals=self.decimals)
+            assert len(values) == 1
+            self.active_preset[0] = values[0]
+        elif command == "SOVP":
+            values = split_data_to_values(data=value_data, width=self.width, decimals=self.decimals)
+            assert len(values) == 1
+            self.active_preset[1] = values[0]
+
+    def handle_gets(self, command):
+        response = bytearray()
+        if command == "GMAX":
+            for value in [self.max_voltage, self.max_current]:
+                response.extend(format_val(val=value, fmt=self.value_format).encode())
+        elif command == "GETS":
+            for value in self.active_preset:
+                response.extend(format_val(val=value, fmt=self.value_format).encode())
+        elif command == "GETD":
+            for value in self.display_values:
+                response.extend(format_val(val=value, fmt=self.value_format).encode())
+        elif command == "GETM":
+            for preset in self.presets:
+                for value in preset:
+                    response.extend(format_val(val=value, fmt=self.value_format).encode())
+        elif command == "GOVP":
+            response.extend(format_val(val=self.active_preset[0], fmt=self.value_format).encode())
+        elif command == "GOCP":
+            response.extend(format_val(val=self.active_preset[1], fmt=self.value_format).encode())
+        return response
 
     def write(self, data: bytes):
         command = data[:4].decode()
         response = bytearray()
+        value_data = data[4:].decode()
 
-        if command == "GMAX":
-            for value in [self.max_voltage,self.max_current]:
-                response.extend(format_val(value, self.value_format).encode())
-
-        if command == "SOUT":
-            # set output status
-            value = int(data[4:].decode())
-            assert value in [OutputStatus.off, OutputStatus.on]
-            self.output_status = value
-
-        elif command == "VOLT":
-            values = split_data_to_values(data[4:].decode(), width=self.width, decimals=self.decimals)
-            assert len(values) == 1
-            self.active_preset[0] = values[0]
-
-        elif command == "CURR":
-            values = split_data_to_values(data[4:].decode(), width=self.width, decimals=self.decimals)
-            assert len(values) == 1
-            self.active_preset[1] = values[0]
-
-        elif command == "GETS":
-            for value in self.active_preset:
-                response.extend(format_val(value, self.value_format).encode())
-
-        elif command == "GETD":
-            for value in self.display_values:
-                response.extend(format_val(value, self.value_format).encode())
-
-        elif command == "GETM":
-            for preset in self.presets:
-                for value in preset:
-                    response.extend(format_val(value, self.value_format).encode())
-
-        elif command == "RUNM":
-            value = int(data[4:].decode())
-            self.active_preset = self.presets[value]
-            self.display_values = self.presets[value]
-
-        elif command == "GOVP":
-            response.extend(format_val(value, self.value_format).encode())
-
-        elif command == "SOVP":
-            values = split_data_to_values(data[4:].decode(), width=self.width, decimals=self.decimals)
-            assert len(values) == 1
-            self.active_preset[0] = values[0]
-
-        elif command == "GOCP":
-            response.extend(format_val(value, self.value_format).encode())
-
-        elif command == "SOCP":
-            values = split_data_to_values(data[4:].decode(), width=self.width, decimals=self.decimals)
-            assert len(values) == 1
-            self.active_preset[1] = values[0]
+        if command in self.set_commands:
+            self.handle_sets(command, value_data)
+        elif command in self.get_commands:
+            response.extend(self.handle_gets(command))
 
         if len(response) > 0:
             self.out_buffer.extend(response)
